65508c82d806acc0b7fdaf9c1b3daad2922979d9
todogeorge <gbt2108@columbia.edu>
2015-09-27 19:56:27 -0400

    index changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,3 +11,10 @@ def index():
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
+    
+@app.route("/getData/")
+def getData():
+    print "getting the data"
+    output = {"out":"This is the data"}
+    return json.dumps(output)
+    
