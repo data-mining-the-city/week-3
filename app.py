dba955d6b321e0924a8098a98be2eb9cdba358ae
ala19901010 <cl3405@columbia.edu>
2015-10-28 02:06:34 +0800

    Fixed the Database bug_Chenxing Li
    Finally get the circles on the map.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
 def getData():
 	
 	client = pyorient.OrientDB("localhost", 2424)
-	session_id = client.connect("root", "admin")
+	session_id = client.connect("root", "network.ssl.keyStorePassword")
 	db_name = "soufun"
 	db_username = "admin"
 	db_password = "admin"
